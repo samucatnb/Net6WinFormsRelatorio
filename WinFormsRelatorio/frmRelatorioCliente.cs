2c26d5c6446ab9fc1212f7cdecb569122dea39e2
Samuel de Morais <samucatnb@gmail.com>
2022-10-10 15:48:13 -0300

    Add project files.

---
+++ WinFormsRelatorio/frmRelatorioCliente.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsRelatorio
+{
+  public partial class frmRelatorioCliente : Form
+  {
+    public frmRelatorioCliente()
+    {
+      InitializeComponent();
+      reportViewer1.LocalReport.ReportEmbeddedResource = "WinFormsRelatorio.relCliente.rdlc";
+      reportViewer1.RefreshReport();
+    }
+  }
+}
